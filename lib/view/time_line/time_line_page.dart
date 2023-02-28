d07407e788f7aefd3d665e939711982c6d117442
seiyu-nico <yuseiishii141@gmail.com>
2023-02-28 21:30:17 +0900

    section2-4

--- lib/view/time_line/time_line_page.dart
+++ lib/view/time_line/time_line_page.dart
@@ -1,8 +1,12 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 
+// Package imports:
+import 'package:intl/intl.dart';
+
 // Project imports:
 import 'package:udemy_flutter_sns/model/account.dart';
+import 'package:udemy_flutter_sns/model/post.dart';
 
 class TimeLinePage extends StatefulWidget {
   const TimeLinePage({super.key});
@@ -22,12 +26,105 @@ class _TimeLinePageState extends State<TimeLinePage> {
     updatedTime: DateTime.now(),
   );
 
+  List<Post> postList = [
+    Post(
+      id: '1',
+      content: 'はじめまして',
+      postAccountId: '1',
+      createdTime: DateTime.now(),
+    ),
+    Post(
+      id: '2',
+      content: 'はじめまして2',
+      postAccountId: '1',
+      createdTime: DateTime.now(),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('タイムライン'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'タイムライン',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          backgroundColor: Theme.of(context).canvasColor,
+          elevation: 1,
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return Container(
+              decoration: BoxDecoration(
+                border: index == 0
+                    ? const Border(
+                        top: BorderSide(
+                          color: Colors.grey,
+                          width: 0,
+                        ),
+                        bottom: BorderSide(
+                          color: Colors.grey,
+                          width: 0,
+                        ),
+                      )
+                    : const Border(
+                        bottom: BorderSide(
+                          color: Colors.grey,
+                          width: 0,
+                        ),
+                      ),
+              ),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 15,
+              ),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 22,
+                    foregroundImage: NetworkImage(myAccount.imagePath),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  myAccount.name,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  '@${myAccount.userId}',
+                                  style: const TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Text(DateFormat('M/d/yy')
+                                .format(postList[index].createdTime!)),
+                          ],
+                        ),
+                        Text(
+                          postList[index].content,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            );
+          },
+          itemCount: postList.length,
+        ));
   }
 }
